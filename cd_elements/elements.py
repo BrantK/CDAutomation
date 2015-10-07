4d552a5cc075faff00bec93ed640681092060396
BrantK <brant@cyberdust.com>
2015-10-06 18:11:10 -0700

    fixed ok button

--- cd_elements/elements.py
+++ cd_elements/elements.py
@@ -141,8 +141,8 @@ class Home:
     def blast_recipient(self, recipient):
         return WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.NAME, recipient)))
 
-    def OK_button(self):
-        return WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.NAME, "OK")))
+    def blast_Ok_button(self):
+        return WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.NAME, "Ok")))
 
     def next_button(self):
         return WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.NAME, "next")))
