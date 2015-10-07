4d7eaea0d3f57cad2959276aa99efedef825b309
BrantK <brant@cyberdust.com>
2015-10-07 13:01:36 -0700

    Cleaned up

--- QuickAccountCreator.py
+++ QuickAccountCreator.py
@@ -12,32 +12,32 @@ account_email = "onboard01@cyberdust.com"
 
 class AccountCreator(unittest.TestCase):
     def test_account_creation(self):
-        sign_up = SignUp()
+        s = SignUp()
         # Creates username and password
         print("\nCreating new account...")
-        sign_up.sign_up_button().click()
-        sign_up.pick_username().send_keys(account_name)
-        sign_up.sign_up_OK().click()
-        sign_up.create_password().send_keys(account_pw)
-        sign_up.confirm_password().send_keys(account_pw)
-        sign_up.password_OK().click()
-        sign_up.birthday().click(), sleep(2)
+        s.sign_up_button().click()
+        s.pick_username().send_keys(account_name)
+        s.sign_up_OK().click()
+        s.create_password().send_keys(account_pw)
+        s.confirm_password().send_keys(account_pw)
+        s.password_OK().click()
+        s.birthday().click(), sleep(2)
 
         # Scrolls through and sets birthday
         for i in range(7):
-            myDriver.driver.scroll(sign_up.bday_scroll_1(), sign_up.bday_scroll_2())
-        sign_up.birthday_set().click()
-        sign_up.birthday_OK().click()
+            myDriver.driver.scroll(s.bday_scroll_1(), s.bday_scroll_2())
+        s.birthday_set().click()
+        s.birthday_OK().click()
 
         # Enters email
-        sign_up.email().send_keys(account_email)
-        sign_up.email_OK().click()
-        sign_up.OK_button().click()
+        s.email().send_keys(account_email)
+        s.email_OK().click()
+        s.OK_button().click()
 
         # Skips remaining on boarding
         for i in range(3):
-            sign_up.skip_button().click()
-        sign_up.done_button().click()
+            s.skip_button().click()
+        s.done_button().click()
 
 
 # ---START OF SCRIPT
