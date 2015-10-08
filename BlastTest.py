43885ddd29b3bd078333e74ddd47a58020f6dd12
BrantK <brant@cyberdust.com>
2015-10-08 16:39:24 -0700

    Added drawing functionality

--- BlastTest.py
+++ BlastTest.py
@@ -1,7 +1,7 @@
 # Automated blast test
 
 import unittest
-import cd_elements.elements as myDriver
+import cd_elements.elements as wd
 from cd_elements.elements import Home
 from selenium.common.exceptions import TimeoutException
 from time import sleep
@@ -13,14 +13,12 @@ blast_text = ""
 class BlastTest(unittest.TestCase):
     def test_blasts(self):
         h = Home()
-        sleep(10)
+        sleep(1)
         h.action_menu().click()
         h.action_menu_media().click()
         h.photo_button().click()
         h.photo_pen().click()
-
-        # draw something
-
+        wd.driver.swipe(200, 450, 500, 800)
         h.photo_add_text().click()
         h.photo_add_text_field().send_keys("Test test test")
         h.done_button().click()
