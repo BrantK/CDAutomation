117728d403a6321dde607ea172fb3c3a9544b23c
BrantK <brant@cyberdust.com>
2015-10-08 10:28:54 -0700

    New test script, not finished yet

---
+++ BlastTest.py
@@ -0,0 +1,32 @@
+# Automated blast test
+
+import unittest
+import cd_elements.elements as myDriver
+from cd_elements.elements import Home
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+
+blast_recipient = ""
+blast_text = ""
+
+
+class BlastTest(unittest.TestCase):
+    def test_blasts(self):
+        h = Home()
+        sleep(10)
+        h.action_menu().click()
+        h.action_menu_media().click()
+        h.photo_button().click()
+        h.photo_pen().click()
+
+        # draw something
+
+        h.photo_add_text().click()
+        h.photo_add_text_field().send_keys("Test test test")
+        h.done_button().click()
+        h.next_button().click()
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(BlastTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
