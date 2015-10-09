5a7973bef4b2dcb2f420ebfc744d7e059d30d230
BrantK <brant@cyberdust.com>
2015-10-08 18:42:58 -0700

    Added drawing functionality for all screen sizes

--- BlastTest.py
+++ BlastTest.py
@@ -16,13 +16,17 @@ class BlastTest(unittest.TestCase):
         sleep(1)
         h.action_menu().click()
         h.action_menu_media().click()
+        h.photo_flip().click()
         h.photo_button().click()
         h.photo_pen().click()
-        wd.driver.swipe(200, 450, 500, 800)
-        h.photo_add_text().click()
-        h.photo_add_text_field().send_keys("Test test test")
-        h.done_button().click()
-        h.next_button().click()
+        # wd.driver.swipe(200, 450, 500, 800) # start x, start y, end x, end y, duration
+        wd.driver.swipe(h.photo_back_button().location['x']+50, h.photo_back_button().location['y']-50, h.photo_pen().location['x'], h.photo_pen().location['y']+50)
+        h.photo_color().click()
+        wd.driver.swipe(h.next_button().location['x']+100, h.next_button().location['y']-50, h.location_button().location['x']+50, h.location_button().location['y']+50)
+        #h.photo_add_text().click()
+        #h.photo_add_text_field().send_keys("Test test test")
+        #h.done_button().click()
+        #h.next_button().click()
 
 
 if __name__ == '__main__':
